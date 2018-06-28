7f15f8e4563b9ea191b27c23f64908ddbfde7463
mariosky <mariosky@gmail.com>
2018-06-28 13:02:49 -0700

    box plots

---
+++ ejemplos/worldcup/bpgoleslv.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('fifa-world-cup/WorldCupMatches.csv')
+goles = map(sum,zip(df['Home Team Goals'], df['Away Team Goals']))
+
+
+
+num_bins = 6
+
+fig, ax = plt.subplots()
+
+# the histogram of the data
+
+
+ax.boxplot(goles,
+        vert=True,  # vertical box alignment
+        patch_artist=True,  # fill with color
+        labels=['Goles'])  # will be used to label x-ticks
+
+ax.set_ylim(-1, 15)
+
+
+# add a 'best fit' line
+ax.set_ylabel('Goles')
+ax.set_title(r'Goles por partido en Copas del Mundo hasta 2014')
+ax.yaxis.grid(True)
+
+# Tweak spacing to prevent clipping of ylabel
+fig.tight_layout()
+plt.show()
